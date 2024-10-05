1709b4ccf74a51d314ebfdd322a12015e5cf460b
Fedorov Semen <FedorovSemen28@yandex.ru>
2024-10-05 08:51:51 +0300

    init logs as di

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
+import logging
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
 import uvicorn
+from fastapi import FastAPI
+from fast_depends import inject, Depends
 
-from core.settings import get_settings
+from core.settings import get_settings, Settings
 
 
+@inject
 @asynccontextmanager
-async def lifespan(app: FastAPI):
+async def lifespan(app: FastAPI, settings: Settings = Depends(get_settings)):
+    logging.basicConfig(
+        level=logging.INFO,
+        format=settings.log.format,
+    )
+    logging.info("Application starts successfully!")
     yield
 
-application = FastAPI()
 
-if __name__ == "__main__":
-    settings = get_settings()
+application = FastAPI(
+    lifespan=lifespan
+)
+
+
+@inject
+def main(settings: Settings = Depends(get_settings)) -> None:
     uvicorn.run(
         app=settings.conf.app,
         host=settings.conf.host,
@@ -21,6 +33,10 @@ if __name__ == "__main__":
         workers=settings.conf.workers,
         reload=settings.conf.reload,
     )
+
+
+if __name__ == "__main__":
+    main()
 
 
 
