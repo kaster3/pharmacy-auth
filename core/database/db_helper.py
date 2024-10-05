a4c9df139907006e153f6dadbb4d9e063606805f
Fedorov Semen <FedorovSemen28@yandex.ru>
2024-10-05 08:52:18 +0300

    create db-helper file and base config

---
+++ core/database/db_helper.py
@@ -0,0 +1,46 @@
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine, async_sessionmaker, AsyncSession
+
+from core.settings import Settings
+
+
+class DataBaseHelper:
+    def __init__(
+            self,
+            url: str,
+            echo: bool,
+            echo_pool: bool,
+            pool_size: int,
+            max_overflow: int,
+    ) -> None:
+        self.engine: AsyncEngine = create_async_engine(
+            url=url,
+            echo=echo,
+            echo_pool=echo_pool,
+            pool_size=pool_size,
+            max_overflow=max_overflow,
+        )
+        self.session_factory: async_sessionmaker[AsyncSession] = async_sessionmaker(
+            bind=self.engine,
+            autoflush=False,
+            expire_on_commit=False,
+
+        )
+
+    async def dispose(self) -> None:
+        return await self.engine.dispose()
+
+    async def session_getter(self) -> AsyncGenerator[AsyncSession, None]:
+        async with self.session_factory() as session:
+            yield session
+
+
+def get_db_helper(settings: Settings) -> DataBaseHelper:
+    return DataBaseHelper(
+        url=str(settings.db.url),
+        echo=settings.db.echo,
+        echo_pool=settings.db.echo_pool,
+        pool_size=settings.db.pool_size,
+        max_overflow=settings.db.max_overflow,
+    )
