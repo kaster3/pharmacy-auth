a7fe6730701e1ed7b40210b632765db85fcb62ec
Fedorov Semen <FedorovSemen28@yandex.ru>
2024-10-07 17:57:03 +0300

    create user model

---
+++ core/database/models/users.py
@@ -0,0 +1,21 @@
+from typing import TYPE_CHECKING
+
+from fastapi_users_db_sqlalchemy import (
+    SQLAlchemyBaseUserTable,
+    SQLAlchemyUserDatabase,
+)
+
+
+from .base import Base
+from core.database.models.mixins.int_id_pk import IntIdPkMixin
+from core.types.user_id import UserIdType
+
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
+
+
+class User(Base, IntIdPkMixin, SQLAlchemyBaseUserTable[UserIdType]):
+
+    @classmethod
+    def get_db(cls, session: "AsyncSession"):
+        return SQLAlchemyUserDatabase(session, cls)
