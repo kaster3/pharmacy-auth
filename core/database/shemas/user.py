ee5fb2066db335989c1bee3451a021433ed18b20
Fedorov Semen <FedorovSemen28@yandex.ru>
2024-10-07 19:48:12 +0300

    create user's dto

---
+++ core/database/shemas/user.py
@@ -0,0 +1,15 @@
+from fastapi_users import schemas
+
+from core.types import UserIdType
+
+
+class UserCreate(schemas.BaseUserCreate):
+    pass
+
+
+class UserRead(schemas.BaseUser[UserIdType]):
+    pass
+
+
+class UserUpdate(schemas.BaseUserUpdate):
+    pass
