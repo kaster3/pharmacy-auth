b03c43497a2c962a722be25c6ff21453a3fb5d8a
Fedorov Semen <FedorovSemen28@yandex.ru>
2024-10-07 16:08:18 +0300

    create global variable and add autonaming of db tables

--- core/settings.py
+++ core/settings.py
@@ -13,6 +13,14 @@ class DataBase(BaseModel):
     pool_size: int
     max_overflow: int
 
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+
 
 class LaunchConfig(BaseModel):
     app: str
@@ -36,6 +44,9 @@ class Settings(BaseSettings):
 
 def get_settings() -> Settings:
     return Settings()
+
+
+settings = get_settings()
 
 
 
