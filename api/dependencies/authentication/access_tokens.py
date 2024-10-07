b55dd69492bb33c0b02d2e16ad26ff9aefab7370
Fedorov Semen <FedorovSemen28@yandex.ru>
2024-10-07 19:47:01 +0300

    di to get access-token-db

---
+++ api/dependencies/authentication/access_tokens.py
@@ -0,0 +1,12 @@
+from typing import Annotated
+
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core import db_helper, AccessToken
+
+
+async def get_access_token_db(
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+):
+    yield AccessToken.get_db(session=session)
