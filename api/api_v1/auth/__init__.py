12715b5363efbb08b41eaa737e2c7417db77c5eb
Fedorov Semen <FedorovSemen28@yandex.ru>
2024-10-08 18:30:37 +0300

    up some routes

---
+++ api/api_v1/auth/__init__.py
@@ -0,0 +1,20 @@
+from fastapi import (
+    APIRouter,
+    Depends,
+)
+from fastapi.security import HTTPBearer
+from core import settings
+from .auth import router as auth_router
+from .users import router as users_router
+
+http_bearer = HTTPBearer(auto_error=False)
+
+router = APIRouter(
+    prefix=settings.api.v1.prefix,
+    dependencies=[Depends(http_bearer)],
+)
+
+for part in (auth_router, users_router):
+    router.include_router(
+        router=part,
+    )
