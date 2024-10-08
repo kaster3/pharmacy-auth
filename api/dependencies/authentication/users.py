5e06f7fe9381be7abff02d348626003fd8950928
Fedorov Semen <FedorovSemen28@yandex.ru>
2024-10-08 13:46:54 +0300

    di to get user from db

---
+++ api/dependencies/authentication/users.py
@@ -0,0 +1,12 @@
+from typing import Annotated
+
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core import db_helper, User
+
+
+async def get_user_db(
+    session: Annotated["AsyncSession", Depends(db_helper.session_getter)]
+):
+    yield User.get_db(session=session)
