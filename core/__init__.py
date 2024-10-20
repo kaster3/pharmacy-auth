f322bfc7605c5d7a36fdd516ecea894e0aa3feb5
Fedorov Semen <FedorovSemen28@yandex.ru>
2024-10-20 17:26:35 +0300

    delete access token

--- core/__init__.py
+++ core/__init__.py
@@ -1,10 +1,9 @@
 __all__ = (
     "settings",
-    "get_settings",
     "db_helper",
     "User",
-    "AccessToken",
 )
 
-from .settings import settings, get_settings
-from .database import db_helper, User, AccessToken
+from .settings import settings
+from .database import db_helper, User
+
