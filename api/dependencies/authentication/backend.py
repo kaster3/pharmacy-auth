822ba93c3d73dbe4ecda9e834566ab3dd965a68e
Fedorov Semen <FedorovSemen28@yandex.ru>
2024-10-20 17:27:22 +0300

    change strategy

--- api/dependencies/authentication/backend.py
+++ api/dependencies/authentication/backend.py
@@ -1,11 +1,11 @@
 from fastapi_users.authentication import AuthenticationBackend
 
 from core.authentication.transport import bearer_transport
-from .strategy import get_database_strategy
+from .strategy import get_jwt_strategy
 
 
 authentication_backend = AuthenticationBackend(
     name="access-tokens-db",
     transport=bearer_transport,
-    get_strategy=get_database_strategy,
+    get_strategy=get_jwt_strategy,
 )
