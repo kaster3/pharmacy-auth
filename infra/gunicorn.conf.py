0ebf97be674c6b270a9910c83b08903b668d9123
Fedorov Semen <FedorovSemen28@yandex.ru>
2024-10-20 20:33:23 +0300

    plus 1 worker

--- infra/gunicorn.conf.py
+++ infra/gunicorn.conf.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 bind = "0.0.0.0:8000"
 # по максимуму использовать процесс это кол-во логических ядер умножить на 2 и + 1
-workers = 1
+workers = 2
 
 env = os.path.join(os.getcwd(), f".env")
 
