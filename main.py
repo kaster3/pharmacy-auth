9b47f0c16967dee62d3b97b0f5634668d1103eee
Fedorov Semen <FedorovSemen28@yandex.ru>
2024-10-20 17:33:14 +0300

    delete inject and add prefix, change logs

--- main.py
+++ main.py
@@ -3,26 +3,26 @@ from contextlib import asynccontextmanager
 
 import uvicorn
 from fastapi import FastAPI
-from fast_depends import inject, Depends
 from fastapi.responses import ORJSONResponse
 
-from core.settings import get_settings, Settings
 from api.api_v1.auth import router as user_auth_router
+from core.settings import settings
+from core.logger_setup import LoggerSetup
 
 
-@inject
+logger_setup = LoggerSetup()
+logger = logging.getLogger(__name__)
+
+
 @asynccontextmanager
-async def lifespan(app: FastAPI, settings: Settings = Depends(get_settings)):
-    logging.basicConfig(
-        level=logging.INFO,
-        format=settings.log.format,
-    )
-    logging.info("Application starts successfully!")
+async def lifespan(app: FastAPI):
+    logger.info("Application starts successfully!")
     yield
-    logging.info("Application ends successfully!")
+    logger.info("Application ends successfully!")
 
 
 application = FastAPI(
+    root_path="/user",
     default_response_class=ORJSONResponse,
     lifespan=lifespan,
 )
@@ -30,11 +30,11 @@ application = FastAPI(
 for router in (user_auth_router,):
     application.include_router(
         router=router,
+        prefix="/user",
     )
 
 
-@inject
-def main(settings: Settings = Depends(get_settings)) -> None:
+if __name__ == "__main__":
     uvicorn.run(
         app=settings.conf.app,
         host=settings.conf.host,
@@ -42,7 +42,3 @@ def main(settings: Settings = Depends(get_settings)) -> None:
         workers=settings.conf.workers,
         reload=settings.conf.reload,
     )
-
-
-if __name__ == "__main__":
-    main()
